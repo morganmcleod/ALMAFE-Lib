ae253d23ef1a40ee30bc54dcbe8a0f5070dbb23c
Morgan McLeod <mmcleod@nrao.edu>
2022-01-28 17:32:19 -0500

    setup.py for v0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ALMAFE-Lib",
-    version="0.0.8",
+    version="0.0.9",
     author="Morgan McLeod",
     author_email="mmcleod@nrao.edu",
     description="Contains reusable tools which are required by other ALMAFE packages.",
