70bd3b70e5c9d55f2ecaa443d2245658598e254f
morganmcleod <mmcleod@nrao.edu>
2023-06-15 16:42:15 -0400

    0.0.13 make Oracle an optional dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ALMAFE-Lib",
-    version="0.0.12",
+    version="0.0.13",
     author="Morgan McLeod",
     author_email="mmcleod@nrao.edu",
     description="Contains reusable tools which are required by other ALMAFE packages.",
@@ -14,8 +14,8 @@ setuptools.setup(
     url="https://github.com/morganmcleod/ALMAFE-Lib",
     packages=setuptools.find_packages(),
     install_requires=['python-dateutil>=2.8.1',
-                      'mysql-connector-python>=8.0.18',
-                      'cx-oracle>=8.1.0'],
+                      'mysql-connector-python>=8.0.18'],
+    extras_require={ 'Oracle': ['cx-oracle>=8.1.0'] },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
